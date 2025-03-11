a5258c6b3cc5bf2e5e4876f221e24854a089fdc2
chandan kumar <chandankumar700451@gamil.com>
2025-03-11 18:29:57 +0530

    update the and redefine the project

--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-        type: String,
-        required: true
-    },
-    description: String,
-    image: {
-        type: String,
-        set: (v) =>v === '' ? 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?cs=srgb&dl=pexels-anjana-c-169994-674010.jpg&fm=jpg' : v 
-    },
-    price: Number,
-    location: String,
-     
+  title: { type: String, required: true },
+  description: String,
+  image: {
+    filename: String,
+    url: String, // Store both filename and url
+  },
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing = mongoose.model('Listing', listingSchema);
+
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
